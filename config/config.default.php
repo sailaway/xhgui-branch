715629e83e29797bbf8b30eaeab7175cb0082632
wangdean <wangdean@guazi.com>
2020-08-07 15:31:35 +0800

    default handler use file

--- config/config.default.php
+++ config/config.default.php
@@ -12,15 +12,15 @@ return array(
     'extension' => 'tideways_xhprof',
 
     // Can be either mongodb or file.
-    /*
+    
+    // 'save.handler' => 'file',
+    // 'save.handler.filename' => dirname(__DIR__) . '/cache/' . 'xhgui.data.' . microtime(true) . '_' . substr(md5($url), 0, 6),
+    
     'save.handler' => 'file',
-    'save.handler.filename' => dirname(__DIR__) . '/cache/' . 'xhgui.data.' . microtime(true) . '_' . substr(md5($url), 0, 6),
-    */
-    'save.handler' => 'mongodb',
 
     // Needed for file save handler. Beware of file locking. You can adujst this file path
     // to reduce locking problems (eg uniqid, time ...)
-    //'save.handler.filename' => __DIR__.'/../data/xhgui_'.date('Ymd').'.dat',
+    'save.handler.filename' => __DIR__.'/../data/xhgui_'.date('Ymd').'.dat',
     'db.host' => 'mongodb://127.0.0.1:27017',
     'db.db' => 'xhprof',
 
